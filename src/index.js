3cf7e3a1aca4eaa04e5125c8681d8c4a3b3fb995
Dark Junior <sfp87@protonmail.com>
2020-12-19 22:38:58 +0300

    add main logic for games

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import promptly from 'promptly';
+
+export default async (question, game) => {
+  console.log('Welcome to the Brain Games!');
+  const name = await promptly.prompt('May I have your name?');
+  console.log(`Hello, ${name}`);
+  console.log(question);
+  for (let i = 2; i >= 0; i -= 1) {
+    const solution = game();
+    const answer = await promptly.prompt('You answer:');
+    if (solution === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;( correct answer '${solution}'`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
