e1e3deef5922e1f19bddc574062b3c08ae1a1804
Dark Junior <sfp87@protonmail.com>
2020-12-22 22:36:40 +0300

    refactoring code

--- games/prime.js
+++ games/prime.js
@@ -4,13 +4,11 @@ const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
   }
-
   return true;
 };
 
