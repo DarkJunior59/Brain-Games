38691024a8007ea39481109c222c8473eef19a23
Dark Junior <sfp87@protonmail.com>
2021-01-06 11:12:30 +0300

    add function gcd

--- games/gcd.js
+++ games/gcd.js
@@ -2,15 +2,16 @@ import randomNumber from '../src/random-number.js';
 
 const question = 'Find the greatest common divisor of given numbers.';
 
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
+
 const game = () => {
   const number1 = randomNumber(100);
   const number2 = randomNumber(100);
-  const gcd = (a, b) => {
-    if (b === 0) {
-      return a;
-    }
-    return gcd(b, a % b);
-  };
   const solution = String(gcd(number1, number2));
   console.log(`Question: ${number1} ${number2}`);
   return solution;
