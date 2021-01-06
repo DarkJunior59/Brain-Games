ee3e3e4b5e384a386657de153d02b7cbadd1a6b6
Dark Junior <sfp87@protonmail.com>
2021-01-06 10:58:55 +0300

    add function isEven

--- games/even.js
+++ games/even.js
@@ -2,9 +2,11 @@ import randomNumber from '../src/random-number.js';
 
 const question = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (num) => num % 2 === 0;
+
 const game = () => {
   const number = randomNumber(100);
-  const solution = (number % 2 === 0) ? 'yes' : 'no';
+  const solution = isEven(number) ? 'yes' : 'no';
   console.log(`Question: ${number}`);
   return solution;
 };
